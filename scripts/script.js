0029f98d7eebf146678cd76ceac20f3cb1f72f82
samussd <101996753+samussd@users.noreply.github.com>
2023-03-31 05:35:44 -0300

    add buttons, slider to change size

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,12 @@
 const canvas = document.querySelector('.canvas');
-const clearBtn = document.querySelector('.options__clear-btn')
+const clearBtn = document.querySelector('.options__clear-btn');
+const eraserBtn = document.querySelector('.options__eraser-btn');
+const colorInput = document.querySelector('.options__color-picker');
+const sizeInput = document.querySelector('.options__size-picker');
+const sizeText = document.querySelector('.options__size-text');
 
 let tiles = document.querySelectorAll('.canvas__tile');
+let backgroundColor = 'white';
 let currentColor = 'black';
 
 function updateCanvasSize(rows, cols) {
@@ -14,6 +19,13 @@ function updateCanvasSize(rows, cols) {
         canvas.appendChild(tile).className = "canvas__tile";
     }
     tiles = document.querySelectorAll('.canvas__tile');
+
+    ['mouseover','mousedown'].forEach(evt => 
+        tiles.forEach(n => n.addEventListener(evt, function(e) {
+            e.preventDefault()
+            colorTile(e);
+        }))
+    );
 }
 
 function colorTile(e) {
@@ -24,10 +36,43 @@ function clearCanvas() {
     tiles.forEach(n => n.style.backgroundColor = 'white')
 }
 
+function changeColor() {
+    currentColor = colorInput.value;
+}
+
+function toggleEraser() {
+    if (!eraserBtn.classList.contains('--active')) {
+        currentColor = backgroundColor;
+    } else {
+        currentColor = colorInput.value;
+    }
+    eraserBtn.classList.toggle('--active');
+}
+
+function changeCanvas() {
+    updateCanvasSize(sizeInput.value, sizeInput.value);
+    changeSizeText();
+}
+
+function changeSizeText() {
+    sizeText.textContent = `${sizeInput.value} x ${sizeInput.value}`
+}
+
 updateCanvasSize(16,16);
 
-clearBtn.onclick = () => clearCanvas();
 
-['mouseover','mousedown'].forEach(evt => 
-    tiles.forEach(n => n.addEventListener(evt, e => colorTile(e)))
-);
+
+clearBtn.onclick = () => clearCanvas();
+eraserBtn.onclick = () => toggleEraser();
+
+// sizeInput.addEventListener('change', changeCanvas());
+// colorInput.addEventListener('change', changeColor());
+
+
+//if clicked outside the canvas, unselect the eraser
+window.addEventListener('click', function(e){   
+    if (!canvas.contains(e.target) && e.target!=eraserBtn){
+        eraserBtn.classList.remove('--active');
+        currentColor = colorInput.value;
+    }
+  });
