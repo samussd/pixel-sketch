a4e763a4be8098f076619b16e074ff09f893ef66
samussd <101996753+samussd@users.noreply.github.com>
2023-04-01 20:51:38 -0300

    fix: fill tool, tool selection buttons

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,7 @@ const canvas = document.querySelector('.canvas');
 const clearBtn = document.querySelector('.options__clear-btn');
 const eraserBtn = document.querySelector('.options__eraser-btn');
 const rainbowBtn = document.querySelector('.options__rainbow-btn');
+const fillBtn = document.querySelector('.options__fill-btn');
 const bgColorInput = document.querySelector('.options__bg-color-picker');
 const brushColorInput = document.querySelector('.options__brush-color-picker');
 const sizeInput = document.querySelector('.options__size-picker');
@@ -13,13 +14,17 @@ let canvasArray = [];
 let canvasSize = 16;
 let backgroundColor = 'white';
 let brushColor = 'black';
-let activeRainbow = false;
+let selectedTool = 'brush';
 let brushSize = 1;
 
 
 function randint(min, max) { // min and max included
-  return Math.floor(Math.random() * (max - min + 1) + min)
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+const HEXtoRGB = (hex) => "rgb(" + hex.match(/[A-Za-z0-9]{2}/g).map(function(v) { return parseInt(v, 16) }).join(", ") + ")";
+
+const randomHEX = () => "#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
 
 /** Returns position of an element in a 2d array */
 function posIn2DArray(array, element){
@@ -49,7 +54,7 @@ function updateCanvasSize(size) {
             ['mouseover','mousedown'].forEach(
                 evtName => tile.addEventListener(evtName, function(e) {
                     e.preventDefault()
-                    colorTile(e);
+                    clickedTile(e);
                 }));
 
             canvas.appendChild(tile)
@@ -71,15 +76,53 @@ function getElementsInBrush(centerElement){
     return elements;
 }
 
-/** Changes color of tile if mouse1 is pressed */
-function colorTile(e) {
+/** Flood fills the array with the tiles */
+function fill(r, c, newColor, current) {
+    if (r<0 || c<0 || r>canvasSize-1 || c>canvasSize-1) return;
+
+    if (canvasArray[r][c].style.backgroundColor !== current || canvasArray[r][c].style.backgroundColor === newColor) return;
+
+    canvasArray[r][c].style.backgroundColor = newColor;
+    canvasArray[r][c].classList.remove('--background');
+
+    //Fill in all four directions
+    fill(r - 1, c, newColor, current);
+    fill(r + 1, c, newColor, current);
+    fill(r, c - 1, newColor, current);
+    fill(r, c + 1, newColor, current);
+}
+
+/** Performs an action on the canvas/tile depending on the current tool */
+function clickedTile(e) {
     if (e.buttons!==1) return;
+
+    if (selectedTool=='fill') {
+        let elementPos = posIn2DArray(canvasArray, e.target);
+        fill(elementPos[0], elementPos[1], brushColor, e.target.style.backgroundColor);
+    }
+    else colorTile(e);
+}
+
+/** Changes color of tile */
+function colorTile(e) {
+    let color;
+    switch (selectedTool) {
+        case 'brush':
+            color = brushColor;
+            break;
+        case 'eraser':
+            color = backgroundColor;
+            break;
+        case 'rainbow':
+            color = `rgb(${randint(0,255)},${randint(0,255)},${randint(0,255)})`;
+            break;
+    }
 
     let elems = getElementsInBrush(e.target);
     for (let i=0, l=elems.length; i < l; i++){
         elems[i].classList.remove("--background")
-        elems[i].style.backgroundColor = 
-            activeRainbow ? `rgb(${randint(0,255)},${randint(0,255)},${randint(0,255)})` : brushColor;
+        elems[i].style.backgroundColor = color;
+        if (selectedTool=='eraser') elems[i].classList.add('--background');
     }
 }
 
@@ -90,7 +133,7 @@ function clearCanvas() {
 
 /** Replaces the current brush color with the picker color */
 function updateBrushColor() {
-    brushColor = brushColorInput.value;
+    brushColor = HEXtoRGB(brushColorInput.value);
 }
 
 /** Replaces the current canvas bg color with the picker color */
@@ -99,20 +142,27 @@ function updateBackgroundColor() {
     root.style.setProperty('--canvas-bg-color', backgroundColor);
 }
 
-/** Toggles the eraser button */
-function toggleEraser() {
-    if (!eraserBtn.classList.contains('--active')) {
-        brushColor = backgroundColor;
-    } else {
-        brushColor = brushColorInput.value;
+/** Toggles a tool button and changes*/
+function toggleTool(selectedBtn, toolName) {
+    resetTools(selectedBtn);
+
+    if (selectedBtn.classList.contains('--active')) {
+        selectedBtn.classList.remove('--active');
+        selectedTool = 'brush';
     }
-    eraserBtn.classList.toggle('--active');
+    else {
+        selectedBtn.classList.add('--active');
+        selectedTool = toolName;
+    }
 }
 
-/** Toggles the rainbow button */
-function toggleRainbow() {
-    activeRainbow = !activeRainbow;
-    rainbowBtn.classList.toggle('--active');
+/** Resets all tools, except the clicked one */
+function resetTools(selectedBtn) {
+    let buttons = [eraserBtn,rainbowBtn,fillBtn];
+    buttons.forEach(btn => {
+        if (btn !== selectedBtn)
+            btn.classList.remove('--active');
+    })
 }
 
 /** Updates the canvas size and size variable */
@@ -126,28 +176,12 @@ function updateSizeText() {
     sizeText.textContent = `${sizeInput.value} x ${sizeInput.value}`
 }
 
-/** Resets all buttons to their */
-function resetButtons() {
-    eraserBtn.classList.remove('--active');
-    brushColor = brushColorInput.value;
-
-    rainbowBtn.classList.remove('--active');
-    activeRainbow = false;
-}
-
 updateCanvasSize(16);
 
-clearBtn.onclick = () => clearCanvas();
-eraserBtn.onclick = () => toggleEraser();
-rainbowBtn.onclick = () => toggleRainbow();
-
-// sizeInput.addEventListener('change', updateCanvas());
-// colorInput.addEventListener('change', updateColor());
+clearBtn.addEventListener('click', () => clearCanvas());
+eraserBtn.addEventListener('click', () => toggleTool(eraserBtn, 'eraser'));
+rainbowBtn.addEventListener('click', () => toggleTool(rainbowBtn, 'rainbow'));
+fillBtn.addEventListener('click', () => toggleTool(fillBtn, 'fill'));
 
 
-//if clicked outside the canvas, unselect the eraser
-window.addEventListener('click', function(e){   
-    if (!(canvas.contains(e.target) || e.target.tagName=="BUTTON")){
-        resetButtons();
-    }
-  });
+
